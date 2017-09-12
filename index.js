dc09b62478a98101ef327c30cd416640b758451f
rlrh1996 <rlrh1996@gmail.com>
2017-09-12 11:19:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ var position = 0;
 function takeANumber(katzDeliLine, name) {
     katzDeliLine.push(name);
     position++;
-    return position + 1;
+    return `Welcome, ${name}. You are number ${position + 1} in line.`;
 }
 
 function nowServing(katzDeliLine) {
     if (katzDeliLine.length === 0) {
         return "There is nobody waiting to be served!";
     } else {
-        return katzDeliLine.shift()
+        var person = katzDeliLine.shift();
+        return `Currently serving ${name}.`;
     }
 }
