c73ef6d3ed1236a0b8894bec7e86ca6a2cc2f3c9
rlrh1996 <rlrh1996@gmail.com>
2017-09-12 11:18:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ var position = 0;
 function takeANumber(katzDeliLine, name) {
     katzDeliLine.push(name);
     position++;
-    return `Welcome, ${name}. You are number ${position + 1} in line.`;
+    return position + 1;
+}
+
+function nowServing(katzDeliLine) {
+    if (katzDeliLine.length === 0) {
+        return "There is nobody waiting to be served!";
+    } else {
+        return katzDeliLine.shift()
+    }
 }
