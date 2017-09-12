bd2bbb333c2933e4dbffd1ede2ea05159a420a71
rlrh1996 <rlrh1996@gmail.com>
2017-09-12 11:14:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+var position = 0;
+
 function takeANumber(katzDeliLine, name) {
     katzDeliLine.push(name)
-    return `Welcome, ${name}. You are number 1 in line.`
+    return `Welcome, ${name}. You are number ${position + 1} in line.`
 }
